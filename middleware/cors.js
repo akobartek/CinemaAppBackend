cace9f7ae2811489e4721ab51b171c15e0ac1b60
Bartek Sokołowski <sokolowskijbartek@gmail.com>
2019-05-15 19:52:06 +0200

    cors2

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,9 @@
 //CORS middleware
 module.exports = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://127.0.0.1:3000, http://localhost:3000"
+  );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
